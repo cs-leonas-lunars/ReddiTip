c9f44e33b5d9a013be5bd9e03469ebaaba3a8d67
Kevin Zieber <zieber.kevinr@gmail.com>
2020-11-24 09:46:37 -0800

    basic layout for extension working

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 import logo from './logo.svg';
+import alien from './alien.svg'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={alien} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Tip your favorite Reddit contributors with bitcoin over the Lightning Network
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://reddit.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Check out Reddit
         </a>
       </header>
     </div>
