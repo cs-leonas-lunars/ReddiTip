da9609808f713347d214d1745099156222874ea1
Jess <jessicaspiezio07@gmail.com>
2020-11-29 21:06:46 -0500

    Styled the extension and fixed reddit tip buttons

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import logo from './logo.svg';
-import alien from './alien.svg'
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={alien} className="App-logo" alt="logo" />
-        <p>
-          Tip your favorite Reddit contributors with bitcoin over the Lightning Network
-        </p>
-        <a
-          className="App-link"
-          href="https://reddit.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Check out Reddit
-        </a>
+        <img id="background" src="/images/topperBackground.gif" />
+        <div id="overlay" onClick={() => toggleMenu(true)}></div>
+        <img id="brandIcon" src="/images/TipJar.png" />
+        <h1 id="logoText">Topper</h1>
+        <p id="paraText">Please Log In With Reddit</p>
+        <button id="login">
+          <div id="circle"></div>
+          <img id="redditIcon" src="/images/reddit.png" />
+          <p id="loginText">Log In</p>
+        </button>
+        <button id="signup">Create An Account</button>
+        <p id="ethereumText">E T H E R E U M · P O W E R E D</p>
+        <button id="menuButton" onClick={() => toggleMenu(false)}>
+          =
+        </button>
+        <div id="menu"></div>
+        <button id="closeMenu" onClick={() => toggleMenu(true)}>
+          +
+        </button>
       </header>
     </div>
   );
+}
+
+function toggleMenu(status) {
+  if (status) {
+    document.getElementById("overlay").style.cssText =
+      "background-color: rgb(0, 0, 0, 0.15)";
+    document.getElementById("menu").style.cssText =
+      "width: 0vw; box-shadow: 0px 0px 0px 0px #000";
+    document.getElementById("closeMenu").style.cssText = "right: -75vw";
+  } else {
+    document.getElementById("overlay").style.cssText =
+      "background-color: rgb(0, 0, 0, 0.6)";
+    document.getElementById("menu").style.cssText =
+      "width: 80vw; box-shadow: -20px 0px 40px -40px #000";
+    document.getElementById("closeMenu").style.cssText = "right: 5vw";
+  }
 }
 
 export default App;
